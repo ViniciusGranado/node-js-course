5d608d789591a67a0b10fe2cb71408b90f23515d
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-11-23 17:39:57 -0300

    Updated app.js

--- node-first-server/app.js
+++ node-first-server/app.js
@@ -24,13 +24,14 @@ const server = http.createServer((req, res) => {
       console.log(body);
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
 
 
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
   }
 
   res.setHeader('Content-Type', 'text/html');
