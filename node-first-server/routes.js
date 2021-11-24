7c1fef0de4a0f60446e6eef9c064283d1dfc04b5
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-11-24 12:20:29 -0300

    Update project

--- node-first-server/routes.js
+++ node-first-server/routes.js
@@ -7,19 +7,21 @@ const requestHandler = (req, res) => {
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
     res.write('</html>');
     return res.end();
-  };
-  
+  }
+
   if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', (chunk) => {
       console.log(chunk);
       body.push(chunk);
     });
-  
-    req.on('end', () => {
+
+    return req.on('end', () => {
       console.log(body);
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
@@ -30,9 +32,9 @@ const requestHandler = (req, res) => {
       });
     });
   }
-  
+
   res.setHeader('Content-Type', 'text/html');
-  
+
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
