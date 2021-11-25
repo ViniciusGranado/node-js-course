29c5c3b8b48d4555d2b0235ebed790b91f602c65
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-11-24 22:13:42 -0300

    Added static serving on pages

--- node-first-express-server/app.js
+++ node-first-express-server/app.js
@@ -8,13 +8,14 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+});
 
 app.listen(3000);
