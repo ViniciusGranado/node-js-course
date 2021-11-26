f9f2e5d92c83dead6779973de52f7e6a12c4c339
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-11-25 23:01:41 -0300

    Added handlebars pages

--- node-first-express-server/app.js
+++ node-first-express-server/app.js
@@ -2,12 +2,15 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
-app.set('view engine', 'pug');
+
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,7 +20,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.render('404', { docTitle: 'Page Not Found' });
+  res.render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
